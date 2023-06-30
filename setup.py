3bdccb26d8e23df4a3dc73a41932f95f9c2c5fa9
agrouaze <antoine.grouazel@gmail.com>
2023-06-30 16:07:13 +0200

    tune requirements for pypi , and install via pip

--- setup.py
+++ setup.py
@@ -10,7 +10,14 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['xsarsea']
+requirements = [
+    'xsarsea',
+    'xsar',
+    'pyyaml',
+    'numpy',
+    'xarray',
+    'scipy'
+                ]
 
 test_requirements = [ ]
 
