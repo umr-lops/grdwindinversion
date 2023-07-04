dd389181ce56b6f4515eb8b1e606bfcaaf47aaa5
grouazel <antoine.grouazel@ifremer.fr>
2023-07-04 17:28:27 +0200

    try to fix issue with version of package

--- grdwindinversion/__init__.py
+++ grdwindinversion/__init__.py
@@ -1,6 +1,12 @@
 __all__ = ['degrade', 'inversion']
+# try:
+#     from importlib import metadata
+# except ImportError: # for Python<3.8
+#     import importlib_metadata as metadata
+# __version__ = metadata.version('grdwindinversion')
+
+from importlib.metadata import version
 try:
-    from importlib import metadata
-except ImportError: # for Python<3.8
-    import importlib_metadata as metadata
-__version__ = metadata.version('grdwindinversion')
+    __version__ = version("grdwindinversion")
+except Exception:
+    __version__ = "999"
