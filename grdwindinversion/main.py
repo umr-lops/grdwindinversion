226dff502d032edac6b22bbc17d509167f61a57a
grouazel <antoine.grouazel@ifremer.fr>
2023-07-10 13:48:33 +0200

    add option for resolution

--- grdwindinversion/main.py
+++ grdwindinversion/main.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
                         required=False, default=None)
     parser.add_argument('--outputdir', required=False, default=OUT_DEFAULT)
     parser.add_argument('--verbose', action='store_true', default=False)
+    parser.add_argument('--resolution',help='output resolution (i.e. posting) in kilometers',required=False,default=25,type=int)
     parser.add_argument('--overwrite', action='store_true', default=False,
                         help='overwrite existing .nc files [default is False]', required=False)
 
@@ -29,7 +30,8 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.INFO, format=fmt,
                             datefmt='%d/%m/%Y %H:%M:%S', force=True)
     t0 = time.time()
-    input_file = args.input_file
+    input_file = args.input_file.rstrip('/')
+    logging.info('input file: %s',input_file)
     if args.config_file is None:
         if 'S1' in input_file:
             config_file = 'config_S1.yaml'
@@ -46,7 +48,7 @@ if __name__ == '__main__':
     out_folder = args.outputdir
 
     out_file = makeL2(input_file, out_folder, config_file,overwrite=args.overwrite)
-    outpath = createLowerResL2(out_file, 25)
+    outpath = createLowerResL2(out_file, resolution=args.resolution)
     # logging.info('%s successfully written', outpath)
     logging.info('current memory usage: %s ', get_memory_usage(var='current'))
     logging.info('done in %1.3f min', (time.time() - t0) / 60.)
